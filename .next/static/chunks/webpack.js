1e50c378e5d3cc84c27e4ec6128c51e8e1879b6d
kumud <kumud.perera@itechro.com>
2022-11-13 13:42:03 +0530

    Second commit

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "0d42d55bb7c3e453b05c";
+/******/ 	var hotCurrentHash = "ccd9c92a60ae31e4cf59";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
